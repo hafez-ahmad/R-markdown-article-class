52986fc3503a18ba7ae1476cb14f93cb7b746cd6
Hafez Ahmad <hafezahmad100@gmail.com>
2022-04-14 15:18:28 -0500

    Script split into three parts

---
+++ Data Preprocessing.R
@@ -0,0 +1,8 @@
+# loading R libraries
+library (raster)       # raster data
+library(rasterVis)   # raster visualization     
+library(sp)          # spatial data processing          
+library(rgdal)       # spatial data processing      
+library(RStoolbox)   # Image analysis
+library(ggplot2)     # plotting
+library(gridExtra)   # plot arrangement
---
+++ Data analysis Classification and Mapping.R
@@ -0,0 +1,12 @@
+library(tidyverse)  # add  data analysis including ggplot
+library (raster)       # raster data
+library(rasterVis)   # raster visualization     
+library(sp)          # spatial data processing          
+library(rgdal)       # spatial data processing      
+library(RStoolbox)   # Image analysis
+library(ggplot2)     # plotting
+library(gridExtra)   # plot arrangement
+library(lubridate)  # date time
+library(ggpubr) # arranging plots
+library(ggspatial) # annotation 
+library(reshape2)
